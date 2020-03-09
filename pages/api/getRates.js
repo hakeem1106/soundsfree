0aa6d7783b7906b9e754d15eda82b5ac10b67738
Gerald Bryant <hakeem1106@gmail.com>
2020-03-09 22:43:04 +0000

    data pulls

--- pages/api/getRates.js
+++ pages/api/getRates.js
@@ -10,17 +10,23 @@ module.exports = (req, res) => {
   })
   */
 
-  const getRates = async()=>{
-   const url = `http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}` 
+  const getRates = async()=>{  
 
-     fetch(url)
+    await axios({
+        method: 'get',
+        url: `http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`,
+        responseType: 'json'
+    })
+    .then(data=> console.log(data))
+/*  
+     await fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => console.log(data.rates))
         
         .catch(err =>{
             res.send(err)
         })
- 
+ */
       }
  getRates()
  res.end()
